23ea8957191122fa08d9cc327f889a7e759f0d16
JabraelNoael <jabraenoael@gmail.com>
2025-10-13 22:19:18 -0700

    Added a comment about .yaml files

--- train.py
+++ train.py
@@ -3,6 +3,6 @@ from ultralytics import YOLO
 
 file_integrity.checkDataSequencing() #If the code breaks at this line it means the contents of data/image and data/labels don't match
 
-model = YOLO("yolov8n.yaml")
+model = YOLO("yolov8n.yaml") #.yaml means entirely untrained model
 
-result = model.train(data="config.yaml",epochs=40)
+result = model.train(data="YOLO_params.yaml",epochs=40)
